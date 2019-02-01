a4ab8a8a797d822295557950e2d81fa320542a14
Alexandre Beaufays <beaufays.alexandre@gmail.com>
2019-02-01 21:06:05 +0100

    Minesweeper is now available in the menu

--- demineur.py
+++ demineur.py
@@ -2,13 +2,8 @@ from random import sample
 from itertools import product
 
 
-number_of_bombs = 6
-number_of_lines = 8
-number_of_rows = 8
-CountEmoji = ['       ',':one:',':two:',':three:',':four:',':five:',':six:',':seven:',':eight:']
+count_emoji = ['       ',':one:',':two:',':three:',':four:',':five:',':six:',':seven:',':eight:']
 
-grid = [x for x in product(range(0,number_of_lines), range(0,number_of_rows))]
-bombs = sample(grid, number_of_bombs)
 
 def count_neighboors(pos, bombs):
     neighboors = [(pos[0] - 1, pos[1] - 1),
@@ -21,14 +16,19 @@ def count_neighboors(pos, bombs):
                   (pos[0] - 1, pos[1] + 1)]
     return sum([(i in bombs) for i in neighboors])
 
-output = ''
-for i in range(number_of_lines):
-    for j in range(number_of_rows):
-        output += '||'
-        if((i,j) in bombs):
-            output += ':boom:'
-        else:
-            output += CountEmoji[count_neighboors((i,j), bombs)]
-        output += '||'
-    output += '\n'
-print(output)
+
+
+def get_minesweeper(nb_bombs, nb_lines, nb_rows):
+    grid = [x for x in product(range(0,nb_lines), range(0,nb_rows))]
+    bombs = sample(grid, nb_bombs)
+    output = ''
+    for i in range(nb_lines):
+        for j in range(nb_rows):
+            output += '||'
+            if((i,j) in bombs):
+                output += ':boom:'
+            else:
+                output += count_emoji[count_neighboors((i,j), bombs)]
+            output += '||'
+        output += '\n'
+    return output
--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 # Work with Python 3.6
 import discord
 from mytoken import TOKEN
-
+from demineur import get_minesweeper
 
 client = discord.Client()
 
@@ -13,6 +13,12 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith("!play"):
+        command = message.content.split()
+        print(command)
+        msg = get_minesweeper(int(command[1]),  int(command[2]), int(command[3]))
         await client.send_message(message.channel, msg)
 
 @client.event
